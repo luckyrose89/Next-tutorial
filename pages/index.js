9428f256c79c819a03a2ff937fc25724a7afa53b
avidrutham <divyamathur89@gmail.com>
2021-04-02 21:40:15 +0530

    add api call to ninjas page and create static paths with id file in ninjas folder

--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,29 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
-        reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
-        laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
-        sequi!
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
-        reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
-        laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
-        sequi!
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
+          reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
+          laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
+          sequi!
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
+          reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
+          laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
+          sequi!
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
